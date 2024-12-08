e6b61b48303d2ef7aa7b66eadfdf54925d53ab64
minhadn <dn0108@hansung.ac.kr>
2024-12-08 21:16:01 +0900

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from dotenv import load_dotenv
+#pip install python-dotenv
+#pip install langchain-openai
+#pip install streamlit
+
+# from dotenv import load_dotenv
+# load_dotenv()
+
 from langchain_openai import ChatOpenAI
+chat_model = ChatOpenAI()
+
+# subject = "AI"
+# result = chat_model.invoke(subject + "에 대한 시를 써줘.")
+# print(result.content)
+
 import streamlit as st
-import os
-
-# 환경 변수 로드
-load_dotenv()
-
-# OpenAI API 키 설정
-openai_api_key = os.getenv('OPENAI_API_KEY')
-chat_model = ChatOpenAI(api_key=openai_api_key)
 
 st.title("인공지능 시인")
 subject = st.text_input("시의 주제를 입력해주세요.")
@@ -16,6 +20,5 @@ st.write("시의 주제 : " + subject)
 
 if st.button("시 작성"):
     with st.spinner("시 작성중 ..."):
-        # 메서드 이름을 generate()로 변경해보세요
-        result = chat_model.generate(prompt=subject + "에 대한 시를 써줘.")
-        st.write(result)
+        result = chat_model.invoke(subject + "에 대한 시를 써줘")
+        st.write(result.content)
